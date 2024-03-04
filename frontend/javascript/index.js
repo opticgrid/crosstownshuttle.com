83200303c25a38ed64af622c9a625e0d3d2e9afd
Jarrett Lusso <jclusso@gmail.com>
2024-03-04 12:56:35 -0500

    Initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,6 @@
+import "$styles/index.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
